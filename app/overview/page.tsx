d4ddf27bb7fd4e2e300bb9471d059dc75d9cc29b
ZikraZana <monozikk13@gmail.com>
2026-07-11 23:42:28 +0700

    second commit

---
+++ app/overview/page.tsx
@@ -0,0 +1,9 @@
+import ContentOverview from "@/components/Overview/ContentOverview";
+
+export default function Overview(){
+    return(
+        <div>
+            <ContentOverview />
+        </div>
+    );
+}
---
+++ components/Overview/ContentOverview.tsx
@@ -0,0 +1,16 @@
+const ContentOverview = () => {
+    return(
+        <>
+            <div className="flex flex-col gap-4">
+                <h1 className="text-2xl font-semibold text-black dark:text-zinc-50">
+                    Overview
+                </h1>
+                <p className="text-base text-zinc-600 dark:text-zinc-400">
+                    This is the overview page. You can add your content here.
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default ContentOverview;
